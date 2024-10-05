5000e08ccedd9296190f6fb95728f71118cf4f7a
prashantsingh019 <ps731278@gmail.com>
2024-10-05 09:17:15 +0530

    did some size adjustments

--- toDo-App/src/components/Navbar.jsx
+++ toDo-App/src/components/Navbar.jsx
@@ -4,20 +4,19 @@ import toDo from "../assets/toDo.png";
 import gitHub from "../assets/github.png";
 import reactLogo from "../assets/reactLogo.png";
 import questMark from "../assets/questMark.png";
-
-
-import { faReact } from "@fortawesome/free-brands-svg-icons";
 import Button from "./Button";
+
 function Navbar() {
   return (
     <div className="navbar">
+
       <div className="logo">
         <img src={toDo} alt="_logo" width="30px" />
-        <div className="name">To Do App</div>
-        <span className="text-green-500">&copy; Managed and developed by Prashant Singh</span>
+        <div className="App_name">To Do App</div>
+        <span className="author text-xl text-[#a3a3a3]">&copy; Managed and developed by Prashant Singh</span>
       </div>
 
-      <div className="nav-links">
+      <div className="nav-links text-[#767676]">
         <Button name="GitHub"  icon={gitHub}/>
         <Button name="Project Link" icon={reactLogo}/>
         <Button name="How to use" icon={questMark}/>
--- toDo-App/src/components/TaskCard.jsx
+++ toDo-App/src/components/TaskCard.jsx
@@ -5,15 +5,17 @@ function TaskCard({data}) {
      
     const handleEdit = () => {
         let input = document.getElementById("task-in")
-        input.value = data.value;
+        input.value = data.toDo;
       }
     const handleDelete = () => {
        
     }
   return (
-    <div className="todo flex justify-between bg-violet-100 my-1 px-1 rounded items-center text-[red]">
-      <input type="checkbox" name="" id="" className="bg-red-600"/>
-      <div className={!data.isDone ? "":"line-through"}>{data.toDo}</div>
+    <div key={data.toDo} className="todo flex justify-between bg-violet-100 my-1 px-1 rounded items-center text-[red]">
+      <div className="flex gap-2 items-center">
+      <input type="checkbox" name="" id="" className="bg-red-600" />
+      <div className={data.isDone ? "line-through":""}>{data.toDo}</div>
+      </div>
       <div className="buttons flex gap-5">
         <button className="bg-slate-500 rounded text-white px-2 cursor-pointer my-2" onClick={handleEdit}>
           Edit
